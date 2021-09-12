91faa984a9dfe82d38cbbcfce03e547edd98be9a
SB <sb@SARARATs-MacBook-Pro.local>
2021-09-11 22:57:32 -0400

    created right-side dashboard

--- client/src/page/profile.js
+++ client/src/page/profile.js
@@ -4,6 +4,7 @@ import '../page/profile.css';
 function profile() {
     return (
         <div>
+            {/* -------left dashboard ------*/}
             <div className="left_dashboard">
                 <div className="profile-image">
                     <img className="image" src="https://dental-smile.net/public/uploads/profile_png_1113533201904221555971977765.png"/>
@@ -20,12 +21,48 @@ function profile() {
                 </div>
                 
             </div>
+
+            {/* -------right dashboard ------*/}
             <div className="right_dashboard">
+              
                 <div className="info">
-                </div>
-                <div className="tag">
+                        <div className="profile-container">
+                            <h2 >Profile information</h2>
+                            <p>First name: Firesname Lastname</p>
+                            <p>Username: Name2021</p>
+                            <p>Birthday: 11/01/2001</p>
+                            <p>Education: <br/>
+                            School Name1 <br/>
+                            <i>Degree / Mayjor | Year</i>
+                            </p>
+                            <p>Skill: HTML5, CSS, JavaScript, React, JSON, Ajax, NodeJS, PHP, C++, Java, Python</p>
+                        </div>
+
+                        <div className="profile-container">
+                            <h2>Contact information</h2>
+                            <p>Email: teach_together@hackathon.com</p>
+                            <p>Phone: +1 (999) - 999 - 9999</p>
+                            <p>Zipcode: 11111</p>
+                        </div>     
                 </div>
 
+                <div className="tag">
+                    <h2 className="tag-head">My interests</h2>
+                    <li className="tag-title">C++</li>   
+                    <li className="tag-title">Photography</li> 
+                    <li className="tag-title">JavaScript</li> 
+                    <li className="tag-title">Camping</li> 
+                    <li className="tag-title">Traveling</li> 
+                    <li className="tag-title">Cooking</li> 
+                    <li className="tag-title">Dancing</li> 
+                    <li className="tag-title">Web Development</li> 
+                    <li className="tag-title">Music</li> 
+                </div>
+
+                <div className="">
+                    <li className="buttons"><button className="save-button">save</button></li>
+                    <li className="buttons"><button className="cancel-button">cancel</button></li>
+                </div>
 
             </div>
             
